4b383fc5db2b4755e22fc4c5bd15be885785e5b1
Jayesh Gupta <141110053+Jayesh445@users.noreply.github.com>
2025-03-11 07:58:23 +0000

    project initialized for backend

---
+++ server/src/test/java/com/secure/FileShareApp/FileShareAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure.FileShareApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileShareAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
