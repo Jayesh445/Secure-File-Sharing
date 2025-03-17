cd84318b54fc43979fd777a645b615162d402cc6
Jayesh445 <141110053+Jayesh445@users.noreply.github.com>
2025-03-18 01:27:14 +0530

    import changes

--- FileShareApp/src/main/java/com/secure/FileShareApp/dto/UploadedFileDto.java
+++ FileShareApp/src/main/java/com/secure/FileShareApp/dto/UploadedFileDto.java
@@ -1,7 +1,6 @@
 package com.secure.FileShareApp.dto;
 
 import com.secure.FileShareApp.entity.UploadedFile;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- FileShareApp/src/main/java/com/secure/FileShareApp/repository/UploadedFileRepository.java
+++ FileShareApp/src/main/java/com/secure/FileShareApp/repository/UploadedFileRepository.java
@@ -3,13 +3,11 @@ package com.secure.FileShareApp.repository;
 import com.secure.FileShareApp.entity.UploadedFile;
 import com.secure.FileShareApp.entity.User;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UploadedFileRepository extends JpaRepository<UploadedFile, String> {
