f0b8aea2741cd0f846d4c3267068f665600c7ad4
Jayesh445 <141110053+Jayesh445@users.noreply.github.com>
2025-03-25 23:36:53 +0530

    both the backend and frontend is deployed

--- FileShareApp/src/main/java/com/secure/FileShareApp/config/SecurityConfig.java
+++ FileShareApp/src/main/java/com/secure/FileShareApp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.secure.FileShareApp.config;
 import com.secure.FileShareApp.security.AuthEntryPointJwt;
 import com.secure.FileShareApp.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -28,11 +29,15 @@ public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    @Value("${FRONTEND_URL}")
+    private String frontendUrl;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/user/**", "/file/**").hasRole("USER")
                         .requestMatchers("/audit/**", "/roles/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
@@ -52,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://secure-file-share-teal.vercel.app",frontendUrl));
         configuration.setAllowCredentials(true);
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setExposedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
