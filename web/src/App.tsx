a5e5019230d55ac2329ef5b0a70e43b10b9e5c82
Jayesh445 <141110053+Jayesh445@users.noreply.github.com>
2025-03-29 17:50:56 +0530

    testing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Pages
@@ -16,8 +15,6 @@ import AdminDashboard from "./pages/Admin";
 import About from "./pages/About";
 import Features from "./pages/Features";
 import ProtectedRoute from "./components/ProtectedRoute";
-
-const queryClient = new QueryClient();
 
 const App = () => {
   return (
