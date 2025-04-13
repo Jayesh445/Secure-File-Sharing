605e064f643da338f2e10e1091a6584082ebeebe
Jayesh445 <141110053+Jayesh445@users.noreply.github.com>
2025-04-13 15:07:40 +0530

    frontend

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base:"/",
   server: {
     cors:{
       origin: "https://secure-file-share-2svw.onrender.com"
