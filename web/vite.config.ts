ca44a8bbd62b23c3ecdba05ea821cca6b04564c5
Jayesh445 <141110053+Jayesh445@users.noreply.github.com>
2025-04-20 19:24:13 +0530

    vercel

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
@@ -12,6 +13,7 @@ export default defineConfig(() => ({
   },
   plugins: [
     react(),
+    vercel()
   ].filter(Boolean),
   resolve: {
     alias: {
